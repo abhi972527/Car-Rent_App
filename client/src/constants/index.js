48ee58388e058942a9a188fe11dffd45ab345691
Abhijeet Kumar <akc972527@gmail.com>
2023-03-20 13:15:28 +0530

    frontend UI

---
+++ client/src/constants/index.js
@@ -0,0 +1,213 @@
+const data = [
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+    {
+        title: "Koenigsegg",
+        carType: "Sport",
+        rentPrice: "80",
+        peopleCapacity: "2",
+        fuelTankCapacity: "90",
+        carGearType: "Manual",
+
+    },
+
+]
+
+const footerData = [
+    {
+        heading: "About",
+        content: ["How it works", "Featured", "Partnership", "Bussiness Relation"]
+    },
+    {
+        heading: "Community",
+        content: ["Events", "Blogs", "Podcast", "Invite a friend"]
+    },
+    {
+        heading: "Socials",
+        content: ["Discord", "Instagram", "Twitter", "Facebook"]
+    },
+]
+
+
+export {
+    data,
+    footerData,
+};
