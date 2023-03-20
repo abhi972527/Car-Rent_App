d8896118c12e07c4230e19dedfef73bdd98b3fcc
Abhijeet Kumar <akc972527@gmail.com>
2023-03-20 21:56:47 +0530

    changed file structure

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+import mongoose from "mongoose";
+// require("dotenv").config();
 
-let url = process.env.DB
+// let url = process.env.DB
 // const connection = async () => {
 //     try {
 //         const conn = await mongoose.connect(url, {
@@ -13,5 +13,17 @@ let url = process.env.DB
 //         console.log("Failed to connect to the database, exiting..", error);
 //     }
 // }
-const connection = mongoose.connect(`${url}`);
-module.exports = { connection };
+// const connection = mongoose.connect(`${url}`);
+// module.exports = { connection };
+
+
+const connection = (url) => {
+    mongoose.set('strictQuery', true);
+    mongoose.connect(url).then(() => {
+        console.log("Successfully connected to the database");
+    }).catch((error) => {
+        console.log("Failed to connect to the database, exiting..", error);
+    })
+}
+
+export default connection;
